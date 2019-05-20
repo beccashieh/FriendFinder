1c36e5628ce03bb8e5e655a060cd7d701759102b
BShieh <rebeccasshieh@gmail.com>
2019-05-20 16:17:13 -0400

    wip: pages mostly setup; localhost working, link paths working. TODO: continue working on functionality of adding new 'friends', create logic for finding matches, and research method for modal to display the matches appropriately

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsArray = [
+    {
+        name: 'Becca',
+        photo: 'https://media.licdn.com/dms/image/C4E03AQHREzs1Rj5FAA/profile-displayphoto-shrink_200_200/0?e=1564012800&v=beta&t=YWg1AuIPgW4hjF6rPyhPvBj_H5-XkTtKfEZcrtABqxA',
+        scores: [
+            5,
+            4,
+            4,
+            4,
+            3,
+            1,
+            4,
+            5,
+            3
+        ]
+    }
+];
+
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,20 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080
+ 
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
